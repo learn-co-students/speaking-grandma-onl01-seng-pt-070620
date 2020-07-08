90ce7a51451963154464cea2d51f5d2626918e9a
Eugene White <eugenewhite1994@gmail.com>
2020-07-08 18:06:09 +0000

    Done.

--- grandma.rb
+++ grandma.rb
@@ -1,13 +1,11 @@
 def speak_to_grandma(phrase)
     if phrase == "I LOVE YOU GRANDMA!"
       return "I LOVE YOU TOO PUMPKIN!"
-    elsif phrase == phrase.downcase
-  		return "HUH?! SPEAK UP, SONNY!"
     elsif phrase == phrase.upcase
       return "NO, NOT SINCE 1938!"
     else
-  	print "Nothing"
+  	return "HUH?! SPEAK UP, SONNY!"
     end
   end
 
-speak_to_grandma('Hi Nana, how are you?')
+
