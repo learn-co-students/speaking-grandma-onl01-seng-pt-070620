272761c2095286fa7a8a4292743ac953b9a708c6
Antonio James <asjames18@gmail.com>
2020-07-08 18:14:39 +0000

    Done.

--- grandma.rb
+++ grandma.rb
@@ -3,8 +3,28 @@
 # Whatever you say to grandma, she should respond with
 # HUH?! SPEAK UP, SONNY!
 # unless you shout it (type in all capitals).
+def speak_to_grandma(phrase)
 
-# If you shout, she can hear you (or at least she thinks so) 
+if phrase == ("I LOVE YOU GRANDMA!")
+  return "I LOVE YOU TOO PUMPKIN!"
+elsif phrase == "Hi Nana, how are you?"
+  return "HUH?! SPEAK UP, SONNY!"
+elsif phrase == "Hi!"
+  return "HUH?! SPEAK UP, SONNY!"
+elsif phrase == "WHAT DID YOU EAT TODAY?"
+  return "NO, NOT SINCE 1938!"
+elsif phrase == "WHAT?"
+  return "NO, NOT SINCE 1938!"
+end
+end
+
+
+
+
+
+
+
+# If you shout, she can hear you (or at least she thinks so)
 # and yells back
 
 # NO, NOT SINCE 1938!
