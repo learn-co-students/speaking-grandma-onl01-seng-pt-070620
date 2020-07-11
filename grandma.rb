97309c24d6f8e998a97211a75eaa8158ff8648b6
Billy Gunn, Jr <bgunnjr@gmail.com>
2020-07-11 22:18:58 +0000

    Done.

--- grandma.rb
+++ grandma.rb
@@ -1,10 +1,9 @@
-def speak_to_grandma
-speak_to_grandma = 'I love you grandma'
-  if speak_to_grandma == 'I LOVE YOU GRANDMA!'
+def speak_to_grandma(phrase)
+  if phrase == 'I LOVE YOU GRANDMA!'
     return HUH?! SPEAK UP, SONNY!
-  elsif SPEAK_TO_GRANDMA || 'I love you grandma!'
+  elsif PHRASE || 'I love you grandma!'
     return NO, NOT SINCE 1938! 
-  else speak_to_grandma.upcase && 'I LOVE YOU GRANDMA!'
+  else phrase.upcase && 'I LOVE YOU GRANDMA!'
     return 'I LOVE YOU TOO PUMPKIN!'
 end
   
